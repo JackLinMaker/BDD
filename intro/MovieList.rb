55eb2180b2ffd91c85e2034d8e50b5398be85df5
jacklinmaker@gmail.com <jacklinmaker@gmail.com>
2013-09-13 10:25:03 +0800

    finish rspec and cucumber scenario

---
+++ intro/MovieList.rb
@@ -0,0 +1,13 @@
+class MovieList
+  def initlaize
+  end
+end
+
+describe MovieList do
+  context "when first created" do
+    it "is empty" do
+      movie_list = MovieList.new
+      movie_list.should be_empty
+    end
+  end
+end
